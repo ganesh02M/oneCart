fdb231234a4c6d566fd5cb7e5570c9d1b65201b3
Ganesh Mishra <mishraganesh9305@gmail.com>
2026-06-08 13:13:03 +0530

    fix: add Vercel URLs to CORS

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174","https://one-cart-ten.vercel.app","https://onecart-admin-beta.vercel.app"],
   credentials: true
 }))
 

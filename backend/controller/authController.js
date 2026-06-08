4889a6b797663fa331016c7e548a84dc090c49cf
Ganesh Mishra <mishraganesh9305@gmail.com>
2026-06-08 13:25:27 +0530

    fix: cookie secure and sameSite for production

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -23,8 +23,8 @@ export const registration = async (req,res) => {
     let token = await genToken(user._id)
     res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
-        sameSite: "Strict",
+        secure:true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
     return res.status(201).json(user)
@@ -50,8 +50,8 @@ export const login = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
-        sameSite: "None",
+        secure:true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
     return res.status(201).json(user)
@@ -88,8 +88,8 @@ export const googleLogin = async (req, res) => {
         let token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json({user, token})
@@ -124,8 +124,8 @@ export const adminLogin = async (req,res) => {
 
     res.cookie("token",token,{
       httpOnly:true,
-      secure:false,
-      sameSite: "Strict",
+      secure:true,
+      sameSite: "none",
       maxAge: 1 * 24 * 60 * 60 * 1000
     });
 
